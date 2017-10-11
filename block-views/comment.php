6f6b52180654add445051046d01c9906606ac5f4
Deadpikle <mikebobby@sbcglobal.net>
2017-10-11 19:40:50 -0400

    Refactor blocks to individual files

---
+++ block-views/comment.php
@@ -0,0 +1,8 @@
+<div class="comment-block codeBlock" id="comment-template">
+    <div class="innerBlock">
+        <textarea class="commentTextArea" rows="4" cols="44" maxlength="1024" ></textarea>			
+    </div>
+    <div class ="blockButtons">
+        <input type="image" onclick="removeBlock(this)" class="blockButton" src="Content/images/TrashButton.png"/>
+    </div>
+</div>
---
+++ block-views/single.php
@@ -0,0 +1,54 @@
+<div class="single-block codeBlock" id="single-template">
+    <div class="innerBlock">
+        <label>Set &nbsp;</label>
+        <select class="option-changer column">
+            <option value='A' selected>A</option>
+            <option value='B'>B</option>
+            <option value='C'>C</option>
+            <option value='D'>D</option>
+            <option value='E'>E</option>
+            <option value='F'>F</option>
+            <option value='G'>G</option>
+            <option value='H'>H</option>
+            <option value='I'>I</option>
+            <option value='J'>J</option>
+            <option value='K'>K</option>
+            <option value='L'>L</option>
+            <option value='M'>M</option>
+            <option value='N'>N</option>
+            <option value='O'>O</option>
+            <option value='P'>P</option>
+        </select>
+        <select class="option-changer row">
+            <option value='1' selected>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+            <option value='6'>6</option>
+            <option value='7'>7</option>
+            <option value='8'>8</option>
+            <option value='9'>9</option>
+            <option value='10'>10</option>
+            <option value='11'>11</option>
+            <option value='12'>12</option>
+            <option value='13'>13</option>
+            <option value='14'>14</option>
+            <option value='15'>15</option>
+            <option value='16'>16</option>
+        </select>
+        <label> square to </label>
+        <select class="option-changer action">
+            <option value="remove">remove color</option>
+            <option value="setto">set to color</option>
+            <option value="randomize" selected>random color</option>
+        </select>
+        <input class="setToColor hidden" type="text" placeholder="#33ccff (Hex Color String)"/>
+        <label>.</label>				
+    </div>
+    <div class ="blockButtons">
+        <input type="image" class="blockButton preview" src="Content/images/EyeButton.png"/>
+        <br>
+        <input type="image" onclick="removeBlock(this)" class="blockButton" src="Content/images/TrashButton.png"/>
+    </div>
+</div>
